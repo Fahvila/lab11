e7079ac396c9cee44374a91f44539d6db5dfb01e
Fahvila <vilasinee_putsiri@cmu.ac.th>
2023-12-22 15:40:25 +0700

    lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<ctime>
 #include<cstdlib>
@@ -9,24 +8,25 @@ string cookie()
 {   
     srand(time(0));
     int grade = rand()%9;
-    if(grade == 0) return "A";
-    else if(grade == 1) return "+B";
-    else if(grade == 2) return "B";
-    else if(grade == 3) return "+C";
-    else if(grade == 4) return "C";
-    else if(grade == 5) return "+D";
-    else if(grade == 6) return "D";
-    else if(grade == 7) return "F";
-    else return "W";
+    if(grade == 0) {return "A";
+    }else if(grade == 1) {return "B+";
+    }else if(grade == 2) {return "B";
+    }else if(grade == 3) {return "C+";
+    }else if(grade == 4) {return "C";
+    }else if(grade == 5) {return "D+";
+    }else if(grade == 6) {return "D";
+    }else if(grade == 7) {return "F";
+    }else {return "W";
+    }
 }
   int main()
 {  
-    cout << "Press Enter 3 times to reveal your future.\n";
+    cout << "Press Enter 3 times to reveal your future.";
     cin.get();
     cin.get();
     cin.get();
 
-    cout <<"You will get "<<cookie()<<" in this 261102";
+    cout <<"You will get "<<cookie()<<" in this 261102. ";
 
     return 0;
 }
